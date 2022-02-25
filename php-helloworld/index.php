d8b084b99299af08c24690608be8573795496d80
Student User <student@workstation.lab.example.com>
2022-02-25 04:08:32 -0500

    update error

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,5 +1,5 @@
 <?php
 print "Hello, World! php version is " . PHP_VERSION . "\n";
-print "Hello chen zhuoheng !"
-print "Again hello chen !!!!!!!"
+print "Hello chen zhuoheng !\n";
+print "Again hello chen !!!!!!!\n";
 ?>
