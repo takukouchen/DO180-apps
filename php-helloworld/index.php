2943a5a54050d4892b1ff32d253c665c2374742d
takukouchen <35527622+takukouchen@users.noreply.github.com>
2022-06-06 15:04:04 +0900

    update for test

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,4 +1,5 @@
 <?php
 print "Hello, World! php version is " . PHP_VERSION . "\n";
 print "Hello, World! Today is 20220511, php version is " . PHP_VERSION . "\n";
+print "Hello, World! Today is 20220606." . PHP_VERSION . "\n";
 ?>
